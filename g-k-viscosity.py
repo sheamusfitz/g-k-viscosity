038aeef59ce441a3b38dd82e4dc09bd8f41c4209
sheamusfitz <47758053+sheamusfitz@users.noreply.github.com>
2021-05-21 16:54:14 -0400

    Revert "Fft"

--- g-k-viscosity.py
+++ g-k-viscosity.py
@@ -1,24 +1,7 @@
 import numpy as np
 import pandas as pd
-# import scipy as sp
+import scipy as sp
 from scipy import signal
-
-import argparse
-
-# I want to add an option to also save the fourier transform of the autocorrelation function.
-# Initialize the parser
-parser = argparse.ArgumentParser()
-# create argument
-parser.add_argument("-f", "--fft", help = "Output fourier transform of autocorrelation function", action="store_true")
-
-
-
-
-# read argument from command line
-args = parser.parse_args()
-
-
-
 
 
 #Aprint('NOTE: this does not yet account for the water viscosity or the thickness of the membrane *relative* to the box.')
@@ -37,7 +20,7 @@ with open(struct_filename) as f:
 #Aprint(sizeline)
 sizes = sizeline.split()
 for i,size in enumerate(sizes):
-  sizes[i] = float(size) * 10**(-9)
+  sizes[i] = float(size)
 
 print("importing...")
 bigpressure = pd.read_csv(xvgname,
@@ -71,6 +54,7 @@ def water_correction(eta_tot_arr, H, h, temp):
   temp:         system temperature
   """
   return(H * eta_tot_arr - (H-h) * water_visc(temp))
+  #TODO I need to actually put this in main() somewhere
 
 
 def main(datapoints = 1000000):
@@ -96,7 +80,7 @@ def main(datapoints = 1000000):
   temp = bigpressure['temp'].mean()
   print("\ntemperature =",temp,"K")
 
-  boxvol = sizes[0]*sizes[1]*sizes[2]
+  boxvol = sizes[0]*sizes[1]*sizes[2] * 10**-27
   print("\nboxvol =", boxvol, "m^3")
   print("\ncalculating autocor")
 
@@ -120,9 +104,7 @@ def main(datapoints = 1000000):
     * (1.38064852 * 10**-23)**(-1) #1/k_B
   )
   
-  # in the following line, the "sizes[2]" part was removed, because that gets
-  # factored in with the 'water_correction()' function.
-  visco_total = viscosityfactor / temp * integrated # * sizes[2]
+  visco_total = viscosityfactor / temp * integrated * sizes[2]
 
   visco_arr = water_correction(visco_total, sizes[2], thickness, temp) 
   
@@ -150,12 +132,6 @@ def main(datapoints = 1000000):
     bigpressure['time (s)'][::nn],
     stress_autocor[::nn],
     visco_arr[::nn],
-    )
-
-  if args.fft:
-    np.savez_compressed(
-      './autocor_fft.npz',
-      np.fft.fft(stress_autocor)
     )
 
 main()
